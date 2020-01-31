1b9afa2b556a7d1da5cc1bcd700763f4869fe112
Chris Kalaf <ckalaf@ibm.com>
2020-01-31 15:13:45 -0600

    Placeholder fix
    Fixed issue where placeholder would persist even as the user types into text box

--- src/Search.js
+++ src/Search.js
@@ -6,11 +6,11 @@ export const Search = (props) => {
   return (
     <div className="mdl-card mdl-shadow--8dp" style={styles.wrapper}>
       <div className="mdl-textfield mdl-js-textfield">
-        <input type="search" onChange={e => setSearchString(e.target.value)} className="mdl-textfield__input" id="searchString" />
-        <label className="mdl-textfield__label" htmlFor="searchString">Pokemon</label>
+        <input type="search" onChange={e => setSearchString(e.target.value)} placeholder="Pokemon" className="mdl-textfield__input" id="searchString" />
+        <label className="mdl-textfield__label" htmlFor="searchString"></label>
       </div>
       <div className="mdl-card__actions mdl-card--border">
-        <button onClick={e => fetchPokemon(searchString)} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Search</button>
+        <button onClick={() => fetchPokemon(searchString)} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Search</button>
       </div>
     </div>
   )
